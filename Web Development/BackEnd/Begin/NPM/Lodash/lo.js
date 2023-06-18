9cae68ce04c1345ef7fd5237964625af7b77af30
supuram <utsa.rkmv@gmail.com>
2023-06-18 08:26:50 +0530

    18th June removing NPM

--- Web Development/BackEnd/Begin/NPM/Lodash/lo.js
+++
@@ -1,4 +0,0 @@
-const a = require('lodash')
-const items = [1,[2,[3,[4,[5]]]]]
-const newItems = a.flattenDeep(items)
-console.log(newItems)
--- Web Development/BackEnd/Begin/NPM/SillyName-1/first.js
+++
@@ -1,9 +0,0 @@
-// var generateName = require('sillyname'); if no import statement then write this line
-import generateName from "sillyname";
-var sillyName = generateName();
-console.log(sillyName)
-
-/*
-You can only use import when you have modified the package.json file, where below the "main": "first.js" you have 
-to write "type": "module"
-*/
