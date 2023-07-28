2be698ed0db87ddb558bd2fb3e27d3003a22ad07
supuram <utsa.rkmv@gmail.com>
2023-07-28 09:34:55 +0530

    28thjuly2023

--- Web Development/reactplusdatabase/src/index.js
+++ Web Development/reactplusdatabase/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './AgainBegin2/PatroPatri/Home/App.js';
+import App from './AgainBegin2/EducationWebsite/App';
 import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
